b2c2bd8008d25c66469b5cd7827f823dc45a75b7
Mark <mark.cherrie.mc@gmail.com>
2023-01-12 16:26:40 +0000

    added constraints

--- server.R
+++ server.R
@@ -9,92 +9,122 @@ library(readxl)
 # server
 shinyServer(function(input,output){
   
-# fetch the uploaded raw data
-file1_data <- reactive({
+  # fetch the uploaded raw data
+  file1_data <- reactive({
     req(input$file1)
     read.csv(input$file1$datapath, header = TRUE, sep = ",")
   })
-
-# fetch the uploaded meta data
-file2_data <- reactive({
-  req(input$file2)
-  read_excel(input$file2$datapath)
-})
-
-typeofdata<-reactive({
-  req(file1_data())
-  typeofdata<-gsub(".csv", "", input$file1$name)
-  typeofdata<-sub('.*\\_', '', typeofdata)
-  typeofdata
-})
-
-
-# reactive meta data
-metadatasubset<-reactive({
   
-  # Meta data
-  req(file2_data())
-  metadata <- file2_data()
+  # fetch the uploaded meta data
+  file2_data <- reactive({
+    req(input$file2)
+    read_excel(input$file2$datapath)
+  })
   
-  # File name
-  req(file1_data())
-  if(typeofdata()=="sen"){
-  nameoffile<-gsub("_sen", "", input$file1$name)
-  }else if(typeofdata()=="pol"){
-  nameoffile<-gsub("_pol", "", input$file1$name)   
-  }else if(typeofdata()=="act"){
-  nameoffile<-gsub("_act", "", input$file1$name)
-  }
-  nameoffile<-gsub(".csv", "", nameoffile)
+  typeofdata<-reactive({
+    req(file1_data())
+    typeofdata<-gsub(".csv", "", input$file1$name)
+    typeofdata<-sub('.*\\_', '', typeofdata)
+    typeofdata
+  })
   
-  # Subset meta data based on file name
-  metadatasubset<-subset(metadata, participantID==as.character(nameoffile))
-  metadatasubset
   
-})
-
-# reactive raw data
-rawdatasubset<-reactive({
+  # reactive meta data
+  metadatasubset<-reactive({
+    req(file1_data())
+    
+    if(grepl("^EP[[:digit:]]_[[:digit:]][[:digit:]]_[[:digit:]][[:digit:]][[:digit:]]_", input$file1$name, ignore.case = FALSE)==T){
+      # Meta data
+      req(file2_data())
+      metadata <- file2_data()
+      
+      if(typeofdata()=="sen"){
+        nameoffile<-gsub("_sen", "", input$file1$name)
+      }else if(typeofdata()=="pol"){
+        nameoffile<-gsub("_pol", "", input$file1$name)   
+      }else if(typeofdata()=="act"){
+        nameoffile<-gsub("_act", "", input$file1$name)
+      }
+      
+      
+      nameoffile<-gsub(".csv", "", nameoffile)
+      
+      # Subset meta data based on file name
+      metadatasubset<-subset(metadata, participantID==as.character(nameoffile))
+      metadatasubset
+    } else {
+      showModal(modalDialog(
+        title = "Incorrect file name format",
+        "The format of the file needs to be '[participantID]_[typeofsensor].csv'. The type of sensor can be sensorbox (_sen), polar (_pol) or axivity (_act).
+       For example, if the participant ID was 'EP2_11_202' and data was from the sensorbox (_sen) the filename would be 'EP2_11_202_sen.csv'.",
+        easyClose = TRUE,
+        footer = "Please amend and re-upload"
+      ))
+    }
+    
+  })
   
-  # get the raw data
-  req(file1_data())
-  rawdata <- file1_data()
-  
-  if(typeofdata()=="sen"){
-  
-    # filter the data
-    rawdatasubset <- rawdata %>% dplyr::filter(timestamp >= as.POSIXct(metadatasubset()$startMeasurement, format="%d-%m-%Y %H:%M", tz="CET") & 
-                                    timestamp <= as.POSIXct(metadatasubset()$stopMeasurement, format="%d-%m-%Y %H:%M", tz="CET"))
+  # reactive raw data
+  rawdatasubset<-reactive({
+    req(file1_data())
     
-    # detect outliers here?
+    if(grepl("^EP[[:digit:]]_[[:digit:]][[:digit:]]_[[:digit:]][[:digit:]][[:digit:]]_", input$file1$name, ignore.case = FALSE)==T){
+      
+      # get the raw data
+      
+      rawdata <- file1_data()
+      
+      validate(
+        need(grepl("^EP", input$file1$name, ignore.case = TRUE)==T, "Please upload data in the following format: participantID_")
+      )
+      
+      if(typeofdata()=="sen"){
+        
+        # filter the data
+        rawdatasubset <- rawdata %>% dplyr::filter(timestamp >= as.POSIXct(metadatasubset()$startMeasurement, format="%d-%m-%Y %H:%M", tz="CET") & 
+                                                     timestamp <= as.POSIXct(metadatasubset()$stopMeasurement, format="%d-%m-%Y %H:%M", tz="CET"))
+        
+        # detect outliers here?
+        
+      } else if(typeofdata()=="pol"){
+        
+        # process the polar data here
+        
+        
+      } else if(typeofdata()=="act"){
+        
+        # process the axivity data here
+        
+      }
+      
+      rawdatasubset
+    } else {
+      showModal(modalDialog(
+        title = "Somewhat important message",
+        "This is a somewhat important message.",
+        easyClose = TRUE,
+        footer = NULL
+      ))
+    }
     
-  } else if(typeofdata()=="pol"){
     
-    # process the polar data here
-    
-    
-  } else if(typeofdata()=="act"){
-    
-    # process the axivity data here
-    
-  }
-
-    rawdatasubset
   })
   
   # Render the raw data table
   output$raw<-renderDataTable({
+    req(rawdatasubset)
     rawdatasubset()
   })
   
   # render the meta data table
   output$meta<-renderDataTable({
+    req(metadatasubset)
     metadatasubset()
   })
   
   
-# Render raw plot
-output$rawplot <- renderPlot(
+  # Render raw plot
+  output$rawplot <- renderPlot(
     
     
     rawdatasubset() %>%
@@ -111,16 +141,16 @@ output$rawplot <- renderPlot(
       scale_x_datetime(breaks = '12 hour', date_labels = '%d/%m %H:%M')
     
   )
-
-# Render downloaded data
-output$downloadData <- downloadHandler(
   
-  filename = function() {
-    paste(gsub(".csv", "", input$file1$name), "_subset.csv", sep="")
-  },
-  content = function(file) {
-    write.csv(rawdatasubset(), file, row.names=F)
-  }
-)
+  # Render downloaded data
+  output$downloadData <- downloadHandler(
+    
+    filename = function() {
+      paste(gsub(".csv", "", input$file1$name), "_subset.csv", sep="")
+    },
+    content = function(file) {
+      write.csv(rawdatasubset(), file, row.names=F)
+    }
+  )
   
 })
